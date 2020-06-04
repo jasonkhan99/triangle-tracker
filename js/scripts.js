4cd137665b644ba0b1f5f45e04899332522d82f8
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-04 15:42:08 -0700

    add if else branch with alerts
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,11 @@ $(document).ready(function() {
     const sideA = parseInt($("#side1").val());
     const sideB = parseInt($("#side2").val());
     const sideC = parseInt($("#side3").val());
-  }
+
+    if (sideA && sideB === sideC) {
+      alert("Your triangle is equilateral");
+    } else if {
+      alert("blah");
+    }
+  });
 });
